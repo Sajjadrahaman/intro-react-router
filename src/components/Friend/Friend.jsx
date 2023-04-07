0e230a153bdb3b1b4d55043de9c9b5b17c272b35
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-04-08 00:12:42 +0600

    navigate on button click

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Friend.css';
-import { Link } from 'react-router-dom';
-const Friend = ({friend}) => {
-    const {id, name, email, website} = friend;
+import { Link, useNavigate } from 'react-router-dom';
+
+const Friend = ({ friend }) => {
+    const { id, name, email } = friend;
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate(`/friend/${id}`);
+    }
+
     return (
         <div className='Friend'>
             <h3>{name}</h3>
             <p>{email}</p>
-            <p><Link to={`/friend/${id}`}>Show Details</Link></p>
+            {/* <p><Link to={`/friend/${id}`}>Show Details</Link></p> */}
+            {/* <Link to={`/friend/${id}`}><button>Show Details</button></Link> */}
+            <button className='btn-details' onClick={handleNavigation}>Show Details</button>
         </div>
     );
 };
