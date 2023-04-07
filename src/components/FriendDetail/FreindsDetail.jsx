701e2e604ba8b0f12166e8cd1d42ea0154232abf
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-04-08 00:17:05 +0600

    add Go Back Button for return previous page

--- src/components/FriendDetail/FreindsDetail.jsx
+++ src/components/FriendDetail/FreindsDetail.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./FriendDetail.css";
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const FreindsDetail = () => {
     const friend = useLoaderData();
+    
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div className='FriendDetails'>
             <h3>Everything About This Person Here:</h3>
@@ -11,6 +17,7 @@ const FreindsDetail = () => {
             <p>Email: {friend.email}</p>
             <p>Phone: {friend.phone}</p>
             <p>Website: {friend.website} </p>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
